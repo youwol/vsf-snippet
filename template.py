440aa78cb8cfbbe853dc0ab01971ada256d79c8c
youwol-guillaume <greinisch@youwol.com>
2023-09-06 15:38:01 +0200

    :arrow_up: `template.py` => upgrade `vsf-core` & `vsf-canvas`

--- template.py
+++ template.py
@@ -18,8 +18,8 @@ folder_path = Path(__file__).parent
 pkg_json = parse_json(folder_path / "package.json")
 
 load_dependencies = {
-    "@youwol/vsf-core": "^0.2.0",
-    "@youwol/vsf-canvas": "^0.2.0",
+    "@youwol/vsf-core": "^0.2.1",
+    "@youwol/vsf-canvas": "^0.2.1",
     "rxjs": "^6.5.5",
     "@youwol/flux-view": "^1.1.0",
     "@youwol/cdn-client": "^2.0.6",
